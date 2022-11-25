0e4fc24d0b0ba1fb93a98a781102dcfcbcf5d2a1
Alexander Vladimir <alexander.muniz@exelaonline.com>
2022-11-24 22:15:10 -0600

    Se creo el proyecto en NET Core 5, Entity Framework, SQL Server y Swagger.

---
+++ FBTarjetas/AplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using FBTarjetas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FBTarjetas
+{
+    public class AplicationDBContext : DbContext
+    {
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDBContext(DbContextOptions<AplicationDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
